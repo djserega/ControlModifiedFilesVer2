1c1a9c06e985c0aeca8e391e71149a844d8ee678
dj_serega <galyuk.sergey@gmail.com>
2018-02-24 19:03:58 +0200

    add class Errors, Subscriber

---
+++ src/ControlModifiedFiles/Errors.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlModifiedFiles
+{
+    internal static class Errors
+    {
+
+        private static string _fileName = GetFileNameFileError();
+      
+        internal static void Save(string message)
+        {
+            bool writeHeader = false;
+
+            FileInfo fileInfo = new FileInfo(_fileName);
+            if (!fileInfo.Exists)
+            {
+                fileInfo.Create();
+                
+                writeHeader = true;
+            }
+
+            using (StreamWriter writer = fileInfo.AppendText())
+            {
+                if (writeHeader)
+                    writer.WriteLine(GetTextMessageError());
+
+                writer.WriteLine(GetTextMessageError(message));
+                writer.Flush();
+            }
+        }
+
+        internal static void Save(Exception ex)
+        {
+            bool writeHeader = false;
+
+            FileInfo fileInfo = new FileInfo(_fileName);
+            if (!fileInfo.Exists)
+            {
+                fileInfo.Create();
+                writeHeader = true;
+            }
+
+            using (StreamWriter writer = fileInfo.AppendText())
+            {
+                if (writeHeader)
+                    writer.WriteLine(GetTextMessageError());
+
+                writer.WriteLine(GetTextMessageError(ex));
+                writer.Flush();
+            }
+        }
+
+        private static string GetFileNameFileError()
+        {
+            return Path.Combine(
+                Environment.CurrentDirectory,
+                $"errors{DateTime.Now.ToString("yyyyMMdd")}.txt");
+        }
+
+        private static string GetTextMessageError()
+        {
+            return "Time ; Message ; Source ; StackTrace";
+        }
+
+        private static string GetTextMessageError(string message)
+        {
+            char separator = ';';
+
+            return $"{DateTime.Now.ToString("HH:mm:ss")}" +
+                $" {separator} " +
+                $"{message}" +
+                $" {separator} " +
+                $" - " +
+                $" {separator} " +
+                $" - ";
+        }
+
+        private static string GetTextMessageError(Exception ex)
+        {
+            char separator = ';';
+
+            return $"{DateTime.Now.ToString("HH:mm:ss")}" +
+                $" {separator} " +
+                $"{ex.Message}" +
+                $" {separator} " +
+                $"{ex.Source}" +
+                $" {separator} " +
+                $"{ex.StackTrace}";
+        }
+
+    }
+}
---
+++ src/ControlModifiedFiles/Subscriber.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ControlModifiedFiles
+{
+    internal class Subscriber
+    {
+        internal void Subscribe(FileSubscriber subscriber)
+        {
+            if (subscriber.Checked
+                && !String.IsNullOrWhiteSpace(subscriber.Path))
+            {
+                FileInfo fileInfo = new FileInfo(subscriber.Path);
+
+                FileSystemWatcher watcher = new FileSystemWatcher(fileInfo.DirectoryName, fileInfo.Name)
+                {
+                    NotifyFilter = NotifyFilters.LastWrite
+                };
+
+                watcher.Changed += Subscribe_Changed;
+                watcher.EnableRaisingEvents = true;
+            }
+        }
+
+        private void Subscribe_Changed(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                //Thread.Sleep(2 * 1000);
+
+                //var keyWatcher = DictionaryWatcher.First(f => f.Key.Path == e.FullPath);
+                //FileSubscriber file = keyWatcher.Key;
+
+                //FileInfo fileInfo = new FileInfo(file.Path);
+                //CreateNewVersionFile(fileInfo, file);
+
+                //var args = new ChangedFileEvent();
+                //if (args != null)
+                //    foreach (EventHandler<ChangedFileEvent> deleg in ChangeFileEvent.GetInvocationList())
+                //        deleg.Invoke(this, args);
+            }
+            catch (Exception ex)
+            {
+                Errors.Save(ex);
+            }
+        }
+    }
+}
