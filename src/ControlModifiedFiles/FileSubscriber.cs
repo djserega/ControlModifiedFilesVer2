74eda949dce02a745f82e9cca42ccfdefe51ff00
dj_serega <galyuk.sergey@gmail.com>
2018-02-25 18:02:19 +0200

    NotifyPropertyChanged all prop

--- src/ControlModifiedFiles/FileSubscriber.cs
+++ src/ControlModifiedFiles/FileSubscriber.cs
@@ -11,24 +11,79 @@ namespace ControlModifiedFiles
 {
     public class FileSubscriber : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region Private fields
+
+        private bool _checked;
+        private string _path;
+        private ulong _size;
+        private string _sizeString;
         private int _version;
         private DateTime _dateMaxVersion;
 
+        #endregion
+
+        #region Properties
 
         [Column("Выбрано", IsOnlyRead = false)]
-        public bool Checked { get; set; }
+        public bool Checked
+        {
+            get { return _checked; }
+            set
+            {
+                if (_checked != value)
+                {
+                    _checked = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
 
         [Column("Путь к файлу", Visible = false)]
-        public string Path { get; set; }
+        public string Path
+        {
+            get { return _path; }
+            set
+            {
+                if (_path != value)
+                {
+                    _path = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
 
         [Column("Имя файла", Visible = false)]
         public string FileName { get; set; }
 
         [Column("Размер (byte)", Visible = false)]
-        public ulong Size { get; set; }
+        public ulong Size
+        {
+            get { return _size; }
+            set
+            {
+                if (_size != value)
+                {
+                    _size = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
 
         [Column("Размер", SortMemberPath = "Size")]
-        public string SizeString { get; set; }
+        public string SizeString
+        {
+            get { return _sizeString; }
+            set
+            {
+                if (_sizeString != value)
+                {
+                    _sizeString = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
 
         [Column("№ версии")]
         public int Version
@@ -61,7 +116,7 @@ namespace ControlModifiedFiles
         [Column("Каталог версий", Visible = false)]
         public string DirectoryVersion { get; set; }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
 
         private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
         {
