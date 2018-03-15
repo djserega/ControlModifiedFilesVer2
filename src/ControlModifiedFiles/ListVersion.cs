4264c58d9a37243ed51edf5f02be0792ffbba5cd
dj_serega <galyuk.sergey@gmail.com>
2018-03-16 01:54:30 +0200

    add new classes, image

---
+++ src/ControlModifiedFiles/ListVersion.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlModifiedFiles
+{
+    public class ListVersion : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool _checked;
+        private string _path;
+        private string _fileName;
+        private DateTime _dateModified;
+
+        public bool Checked
+        {
+            get { return _checked; }
+            set
+            {
+                if (_checked != value)
+                {
+                    _checked = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Path
+        {
+            get { return _path; }
+            set
+            {
+                if (_path != value)
+                {
+                    _path = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string FileName
+        {
+            get { return _fileName; }
+            set
+            {
+                if (_fileName != value)
+                {
+                    _fileName = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        
+        public DateTime DateModified
+        {
+            get { return _dateModified; }
+            set
+            {
+                if (_dateModified != value)
+                {
+                    _dateModified = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public ListVersion(string path, string fileName, DateTime dateModified)
+        {
+            _path = path;
+            _fileName = fileName;
+            _dateModified = dateModified;
+        }
+
+    }
+}
