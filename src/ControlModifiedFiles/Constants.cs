435ca3b6fcf9f073de2e72383957c22fa068304d
dj_serega <galyuk.sergey@gmail.com>
2018-04-21 14:36:48 +0300

    Create class constants

---
+++ src/ControlModifiedFiles/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlModifiedFiles
+{
+    internal static class Constants
+    {
+        internal static readonly string prefixVersion = "{version";
+        internal static readonly string postfixVersion = "}";
+    }
+}
